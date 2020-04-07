4fde94cc8b20f1797740795280ce51cb0157f2f4
Muhammad Hifzhan Silmi <56281943+Akhdan26@users.noreply.github.com>
2020-04-07 10:50:40 +0700

    Initial commit

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil Saya</title>
+</head>
+<body>
+    <h1>Profil Saya</h1>
+    <p>Perkenalkan saya Hifzhan, Saya seorang mahasiswa JTI Polinema.</p>
+</body>
+</html>
